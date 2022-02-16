e322fb5bb008e05e895903cf6b9f9932e7d0ea79
manojchaitanya99@gmail.com <chaitanya-1006>
2022-02-16 09:26:46 +0530

    learned routing

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ const express=require('express');
 
 const app=express();
 
-app.use((req,res,next)=>{
-    console.log("I am in middleware!");
-    next(); // Allows the request to continue to the next middleware
+app.use('/add-product',(req,res,next)=>{
+    console.log("I am in another middleware!");
+    res.send('<h1>Add Product Page<h1>');
 })
 
-app.use((req,res,next)=>{
+
+app.use('/',(req,res,next)=>{
     console.log("I am in another middleware!");
     res.send('<h1>Hello  From Express<h1>');
 })
